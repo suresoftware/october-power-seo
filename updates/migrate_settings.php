a518962435ff34b9fb3dff9e12342ffda50e4a24
Nicholas Humphries <nicholas.humphries0@gmail.com>
2019-01-11 10:07:07 +1300

    Added migration to get settings from old plugin

---
+++ updates/migrate_settings.php
@@ -0,0 +1,29 @@
+<?php namespace SureSoftware\PowerSEO\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+use System\Classes\PluginManager;
+class MigrateSettings extends Migration
+{
+
+    public function up()
+    {
+        // migrate the settings from the old plugin if it exists
+        $settings = DB::table('system_settings')
+            ->select('value')
+            ->where('item', 'anandpatel_seoextension_settings')
+            ->first();
+
+        if($settings) {
+            DB::table('system_settings')->insert([
+                'item' => 'suresoftware_powerseo_settings',
+                'value' => $settings->value
+            ]);
+        }
+    }
+
+    public function down()
+    {
+        //do nothing, if rolling back there is no need to remove the settings here
+    }
+}
